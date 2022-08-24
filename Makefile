d54831989cb950b0f3a564ea08be21fe31fa6b0a
MCApollo <34170230+MCApollo@users.noreply.github.com>
2022-08-24 12:51:56 -0500

    .

--- Makefile
+++ Makefile
@@ -1,22 +1,15 @@
-ARCHS=arm64 armv7 armv7s
+TARGET := iphone:clang:latest:7.0
+ARCHS := arm64
 
-TARGET=iphone::7.1
-
-ADDITIONAL_OBJCFLAGS = -fobjc-arc
-
-THEOS_DEVICE_IP=192.168.1.108
-
-include theos/makefiles/common.mk
+include $(THEOS)/makefiles/common.mk
 
 TOOL_NAME = proxytool
 
 proxytool_FILES = main.mm WiFiProxy.m
-
+crane-cli_CFLAGS = -fobjc-arc
 proxytool_FRAMEWORKS = Foundation SystemConfiguration
-
+proxytool_CODESIGN_FLAGS = -Sentitlements.plist
 proxytool_LDFLAGS = -undefined dynamic_lookup
+proxytool_INSTALL_PATH = /usr/local/bin
 
 include $(THEOS_MAKE_PATH)/tool.mk
-
-before-package::
-	ldid -S./Ent.plist $(THEOS_STAGING_DIR)/usr/bin/$(TOOL_NAME);
--- main.mm
+++ main.mm
@@ -1,21 +1,29 @@
 #import <Foundation/Foundation.h>
 #import "WiFiProxy.h"
+#include <stdio.h>
 
 int main(int argc, char **argv, char **envp) {
-	WiFiProxy *proxy = [WiFiProxy sharedInstance]; 
+	WiFiProxy *proxy = [WiFiProxy sharedInstance];
+
+  if (argc < 4 && argc != 2) {
+    fprintf(stderr, "proxytool (1|0) <host> <port>\n");
+
+    return 1;
+  }
 
 	int proxyMode = 0;	//0: no proxy;1: https proxy
 	if (argc >= 2) {
 		proxyMode = atoi(argv[1]);
 	}
-	NSString *host = @"192.168.1.111";
-	int port = 1111;
-	if (argc >= 4) {
-		host = [NSString stringWithUTF8String:argv[2]];
-		port = atoi(argv[3]);
-	}
-	
-	[proxy setProxy:host port:port mode:proxyMode];
+
+  if (proxyMode == 0) {
+    [proxy setProxy:@"" port:1 mode:proxyMode];
+  } else {
+    NSString *host = [NSString stringWithUTF8String:argv[2]];
+    int port = atoi(argv[3]);
+    
+    [proxy setProxy:host port:port mode:proxyMode];
+  }
 
 	return 0;
 }
